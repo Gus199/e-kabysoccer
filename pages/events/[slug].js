d167307c581f0bff871e154fd2936e1193010c3f
Amar <augustine2903a@gmail.com>
2022-08-15 15:48:31 -0400

    add more to Details page ,slug

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,57 @@
-import Layout from "@/components/Layout"
+/* eslint-disable jsx-a11y/alt-text */
+import Layout from "@/components/Layout";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import data from "../../utils/data";
+import Link from "next/link";
+import styles from "@/styles/Event.module.css";
 
 function EventPage() {
-   
+  const { query } = useRouter();
+  const { slug } = query;
+  const event = data.events.find((x) => x.slug === slug);
+  if (!event) {
+    return <div>Produt Not Found</div>;
+  }
+  const deleteEvent = (e) => {
+    console.log("Delete");
+  };
   return (
-      <Layout>
-    <h1>My Event</h1>
-    
+    <Layout title={event.name}>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${event.slug}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes />
+            Delete Event
+          </a>
+        </div>
+        <span>
+          {event.date} at {event.time}
+        </span>
+        <h1>{event.name}</h1>
+        {event.image && (
+          <div className={styles.image}>
+            <Image src={event.image} width={960} height={600} />
+          </div>
+        )}
+       <h3>Description:</h3>
+       <p>{event.description}</p>
+       <h3>Venue:{event.venue}</h3>
+       <p>{event.address}</p>
+       <Link href='/events'>
+         <a className={styles.back}>
+          {'<'} Go Back
+         </a>
+       </Link>
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default EventPage
+export default EventPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Layout from '@/components/Layout'
 function EventsPage() {
   return (
-    <div>EventsPage</div>
+    <Layout>EventsPage</Layout>
   )
 }
 
